5b26f08426706a55d5189034cf7a0852746bc0f0
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2022-12-24 22:00:48 +0300

    Create py-set-union.py

---
+++ week1/py-set-union.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == '__main__':
+    n = input().split()
+    st = set(map(str, input().split()))
+    b = input().split()
+    nxt = set(map(str, input().split()))
+    res = st.union(nxt)
+    print(len(res))
